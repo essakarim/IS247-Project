4a91ce881f62ee67e090942a03ee38b9ba9772e3
cm459 <connor13@umbc.edu>
2026-09-20 00:45:01 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,26 @@
+
+public class Player { //Player inventory class
+	
+	//Variable creation
+	public boolean ductTape;
+	public boolean woodBoard;
+	public boolean blanket;
+	
+
+public Player() { //Initializing our class
+		
+		
+	}
+
+	
+public Player(boolean ductTape1, boolean woodBoard1, boolean blanket1) { //Adding objects to initialize
+	
+	//Assigning data
+	this.ductTape = ductTape1;
+	this.woodBoard = woodBoard1;
+	this.blanket = blanket1;
+	
+	
+	}
+
+}
